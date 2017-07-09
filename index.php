635824a686209f9f4a703b12dfeb5327cd946fc9
xinxin <13552028675@163.com>
2017-07-09 19:40:12 +0800

    初始化项目

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // 后端路由(分发URL请求)
+    $pathname = "index";
+    $filename = "index";
+
+    // var_dump($_SERVER);
+    if(isset($_SERVER['PATH_INFO'])){
+        $urlpath = $_SERVER['PATH_INFO'];
+        
+        $str = substr($urlpath,'1');
+        $arr = explode("/",$str);
+        if(count($arr == 2)){
+            $pathname = $arr[0];
+            $filename = $arr[1];
+        }
+    }else{
+        $filename = "login";
+    }
+    include('./views/'.$pathname.'/'.$filename.'.html');
